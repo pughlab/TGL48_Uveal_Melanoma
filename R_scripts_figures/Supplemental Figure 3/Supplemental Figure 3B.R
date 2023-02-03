7fbadd81709baaaa793c19f5200352f926594ca1
derekwong90 <78445989+derekwong90@users.noreply.github.com>
2023-02-03 10:09:35 -0500

    Update Supplemental Figure 3B.R

--- R_scripts_figures/Supplemental Figure 3/Supplemental Figure 3B.R
+++ R_scripts_figures/Supplemental Figure 3/Supplemental Figure 3B.R
@@ -7,9 +7,9 @@ library(matrixStats)
 library(lemon)
 
 ### Set paths
-path <- "/Users/derekwong/OneDrive - UHN/Post-Doc/Uveal_Melanoma_Project"
-outdir <- "/Users/derekwong/Google Drive/Post-Doc/Uveal Melanoma/Manuscript/Figures/Supplemental Figure 12"
-healthy <- "/Users/derekwong/OneDrive - UHN/Post-Doc/Healthy_control_cohorts/combined_cohort/griffin/uveal"
+path <- ""
+outdir <- ""
+healthy <- ""
 
 eye <- file.path(path, "griffin/uveal/TGL48_UVM_griffin_corrected_Eye.txt")
 samples <- file.path(path, "sample_list.txt")
